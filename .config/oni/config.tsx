9bc9b67d9bebedc7c04239504feb31d768a6ed0f
Kagan <kgnugur@gmail.com>
2018-12-19 23:24:38 +0300

    U+1F42E

---
+++ .config/oni/config.tsx
@@ -0,0 +1,62 @@
+import * as React from "react"
+import * as Oni from "oni-api"
+
+export const activate = (oni: Oni.Plugin.Api) => {
+  console.log("config activated")
+
+  // Input
+  //
+  // Add input bindings here:
+  //
+  oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+
+  //
+  // Or remove the default bindings here by uncommenting the below line:
+  //
+  // oni.input.unbind("<c-p>")
+}
+
+export const deactivate = (oni: Oni.Plugin.Api) => {
+  console.log("config deactivated")
+}
+
+export const configuration = {
+  "browser.defaultUrl": "https://google.com",
+  "ui.fontSize": "15px",
+  "sidebar.width": "12em",
+  //"oni.useDefaultConfig": true,
+  //"oni.bookmarks": ["~/Documents"],
+  //"oni.loadInitVim": false,
+  "editor.fontSize": "19px",
+  "editor.fontFamily": "Fira Mono",
+
+  // UI customizations
+  "ui.animations.enabled": true,
+  "ui.fontSmoothing": "auto",
+  "statusbar.priority": {
+    "oni.status.workingDirectory": 0,
+    "oni.status.linenumber": 2,
+    "oni.status.gitHubRepo": 0,
+    "oni.status.mode": 0,
+    "oni.status.filetype": 1,
+    "oni.status.git": -1,
+  },
+
+   activate,
+    "oni.hideMenu"             : "hidden", // Hide top bar menu
+    "oni.loadInitVim"          : true, // Load user's init.vim
+    "oni.useDefaultConfig"     : false, // Do not load Oni's init.vim
+    "ui.colorscheme"           : "n/a", // Load init.vim colorscheme, remove this line if wants Oni's default
+    "autoClosingPairs.enabled" : false, // disable autoclosing pairs
+    "commandline.mode"         : false, // Do not override commandline UI
+    "wildmenu.mode"            : false, // Do not override wildmenu UI,
+    "tabs.mode"                : "native", // Use vim's tabline, need completely quit Oni and restart a few times
+    "statusbar.enabled"        : false, // use vim's default statusline
+    "sidebar.enabled"          : false, // sidebar ui is gone
+    "sidebar.default.open"     : false, // the side bar collapse
+    "learning.enabled"         : false, // Turn off learning pane
+    "achievements.enabled"     : false, // Turn off achievements tracking / UX
+    "editor.typingPrediction"  : false, // Wait for vim's confirmed typed characters, avoid edge cases
+    "editor.textMateHighlighting.enabled" : false, // Use vim syntax highlighting
+
+}
